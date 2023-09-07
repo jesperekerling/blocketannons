29aa9d2507992b92122b6e923eb3e9dc5489620a
jesperekerling <jesper@forme.se>
2023-09-07 11:10:43 +0200

    Test av if-sats

--- task.js
+++ task.js
@@ -1,11 +1,14 @@
 brand = "Audi"
 modelYear = 2000
 model = "A3"
-currentMileage = 8000
+currentMileage = 3000
 bigServiceMileage = 6000
 
-console.log(`You own a ${brand} ${model} that has been driven for ${currentMileage} km`)
-
+let message = `You own a ${brand} ${model} that has been driven for ${currentMileage} km`
+console.log(message);
 if (currentMileage > bigServiceMileage) {
     console.log(`Time for a big service`)
+}
+else {
+    console.log(`${bigServiceMileage-currentMileage} km left until big service`)
 }
