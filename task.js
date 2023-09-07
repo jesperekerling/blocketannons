93c2c29081045850bcde55960a5bfa21f64fc955
jesperekerling <jesper@forme.se>
2023-09-07 11:05:17 +0200

    Nytt js

---
+++ task.js
@@ -0,0 +1,11 @@
+brand = "Audi"
+modelYear = 2000
+model = "A3"
+currentMileage = 8000
+bigServiceMileage = 6000
+
+console.log(`You own a ${brand} ${model} that has been driven for ${currentMileage} km`)
+
+if (currentMileage > bigServiceMileage) {
+    console.log(`Time for a big service`)
+}
